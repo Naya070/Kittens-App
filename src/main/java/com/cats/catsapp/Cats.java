f004a5bc09760f55dc286d512f101131c7bf9e57
Naya070 <nayarith070@gmail.com>
2023-06-05 16:23:53 -0400

    first commit

---
+++ src/main/java/com/cats/catsapp/Cats.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cats.catsapp;
+
+/**
+ *
+ * @author naya
+ */
+public class Cats {
+    int id;
+    String url;
+    String apikey = "live_iKHct5GJDB5cUJ13vz6O1Oxmxgmh2HUtSWNHyxeB16IMafy11xNvVIVZwkptiJEc";
+    String image;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getApikey() {
+        return apikey;
+    }
+
+    public void setApikey(String apikey) {
+        this.apikey = apikey;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
---
+++ src/main/java/com/cats/catsapp/CatsApp.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.cats.catsapp;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author naya
+ */
+public class CatsApp {
+
+    public static void main(String[] args) {
+        int optionMenu = -1;
+        String[] buttons = {"1. See cats", "2. Exit"};
+        
+        do{
+            //Main menu
+            String option = (String) JOptionPane.showInputDialog(null, 
+                    "Kitten Java", "Main Menu", 
+                    JOptionPane.INFORMATION_MESSAGE, null,
+                    buttons, buttons[0]);
+            
+            for(int i=0; i < buttons.length; i++){
+                if(option.equals(buttons[i])){
+                    optionMenu = i;
+                }
+            }
+            
+            switch(optionMenu){
+                case 0:
+                    CatsService.seeCats();
+                    break;
+                default:
+                    break;
+            }
+            
+        }while(optionMenu != 1);
+    }
+}
